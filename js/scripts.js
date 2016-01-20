c4ad519a3b2ace592243692b12759cdaeb227025
aaron gershenzon and jared beckler <aaron@urbankayaks.com>
2016-01-20 15:42:29 -0800

    add functionality that takes entire sentence and translates into pig latin

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,41 @@
-var translator = function(string){
+var translator = function(word){
   var suffix = "ay";
+  var sentence = word.split(" ");
+  var finalResult = [];
+  sentence.forEach(function(string){
 
-  if (string[0].match(/[aeiou]/)){
-    return string + suffix;
-  } else if (string.substr(0,2) === "qu"){
-    string = string.replace("qu", "");
-    return string + "qu" + suffix;
-  } else if (string.substr(0,3) === "squ"){
-    string = string.replace("squ", "");
-    return string + "squ" + suffix;
-  } else {
-    string = string.split("");
-    while (string[0].match(/[b-df-hj-np-tv-yz]/)){
-    string.push(string[0])
-    string.shift(string[0])
-  }
-  string = string.join("");
-  string = string + suffix;
-  return string
-  }
-}
+    if (string[0].match(/[aeiou]/)){
+      string = string + suffix;
+    }
+    else if (string.substr(0,2) === "qu"){
+      string = string.replace("qu", "");
+      string = string + "qu" + suffix;
+    }
+    else if (string.substr(0,3) === "squ"){
+      string = string.replace("squ", "");
+      string = string + "squ" + suffix;
+    }
+    else {
+      string = string.split("");
+      while (string[0].match(/[b-df-hj-np-tv-yz]/)){
+      string.push(string[0])
+      string.shift(string[0])
+      }
+      string = string.join("");
+      string = string + suffix
+    };
+    finalResult.push(string);
+  });
+  var results = finalResult.join(" ");
+  return results;
+};
+
 
 $(document).ready(function(){
   $("form#translator").submit(function(event){
     var string = $("input#string").val();
     var result = translator(string);
-    
+
     $("#result").text(result);
     $("#result").show();
 
--- specs/specs.js
+++ specs/specs.js
@@ -15,4 +15,8 @@ describe("translator", function(){
   it("check to see if it starts with a consonant. if true, move consecutive consonants to end until you hit a vowel and add 'ay'", function(){
     expect(translator("llama")).to.equal("amallay");
   });
+
+  it("takes a sentence and splits each word into a string and runs all functions above", function(){
+    expect(translator("good luck")).to.equal("oodgay ucklay");
+  });
 });
